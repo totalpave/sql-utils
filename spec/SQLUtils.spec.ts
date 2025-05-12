d7f34fee346b564c356b6bda0f8c943205f08796
Tyler Breau <tylerbreau@hotmail.com>
2025-05-12 15:02:10 -0300

    Corrected SQLUtils using improper date string format. The format technically works but doesn't retain timezone information, which can cause problems if the environment is not UTC. Code now uses ISO String format (Date.toISOString). Added jsdocs to SQLUtils and toDatetime. SQLUtils is now documented as a MySQL utility class.

--- spec/SQLUtils.spec.ts
+++ spec/SQLUtils.spec.ts
@@ -5,42 +5,42 @@ describe('SQLUtils', () => {
     describe('SQLUtils.toDatetime', () => {
         it('SQLUtils.toDatetime', () => {
             let date = new Date('2020-07-16T17:30:53.824Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16 17:30:53.824');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16T17:30:53.824Z');
         });
         
         it('< 100 ms date', () => {
             let date = new Date('2020-07-16T17:30:53.024Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16 17:30:53.024');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16T17:30:53.024Z');
         });
     
         it('< 10 ms date', () => {
             let date = new Date('2020-07-16T17:30:53.004Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16 17:30:53.004');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16T17:30:53.004Z');
         });
     
         it('Leading 0s for months', () => {
             let date = new Date('2020-07-16T17:30:53.004Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16 17:30:53.004');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-16T17:30:53.004Z');
         });
     
         it('Leading 0s for days', () => {
             let date = new Date('2020-07-06T17:30:53.004Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06 17:30:53.004');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06T17:30:53.004Z');
         });
     
         it('Leading 0s for hours', () => {
             let date = new Date('2020-07-06T07:30:53.004Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06 07:30:53.004');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06T07:30:53.004Z');
         });
     
         it('Leading 0s for minutes', () => {
             let date = new Date('2020-07-06T17:03:53.004Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06 17:03:53.004');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06T17:03:53.004Z');
         });
     
         it('Leading 0s for seconds', () => {
             let date = new Date('2020-07-06T17:30:03.004Z');
-            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06 17:30:03.004');
+            expect(SQLUtils.toDatetime(date)).toBe('2020-07-06T17:30:03.004Z');
         });
     });
     
@@ -117,7 +117,7 @@ describe('SQLUtils', () => {
         });
 
         it('Date', () => {
-            expect(SQLUtils.toValue(new Date(0))).toBe('"1970-01-01 00:00:00.000"');
+            expect(SQLUtils.toValue(new Date(0))).toBe('"1970-01-01T00:00:00.000Z"');
         });
     });
 });
